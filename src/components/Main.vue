8a513067c950b041f16bd1f90449bbf8925c2349
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-12-19 21:57:36 +0900

    include ocr lib

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,38 +8,60 @@
       name="img"
       id="img"
       accept="image/png, image/jpeg"
-      @change="uploadImg($event)"
+      @change="uploadImg"
     />
-    <img id="img-display" :src="previewImage" />
+    <button @click="runOcr" v-show="previewImage">run OCR</button>
+    <div id="result">
+      <div>
+        <h2>Uploaded image</h2>
+        <img id="img-display" :src="previewImage" />
+      </div>
+      <div>
+        <h2>Recognized text</h2>
+        <p>{{ ocrText }}</p>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-let previewImage = null;
+import { ref } from "vue";
 import { createWorker } from "tesseract.js";
+
+let previewImage: any = ref(null);
+let ocrText = ref("");
+
 const uploadImg = (e) => {
   const image = e.target.files[0];
   const reader = new FileReader();
   reader.readAsDataURL(image);
   reader.onload = (e) => {
-    previewImage = e.target.result;
+    previewImage.value = e.target.result;
   };
 };
+
 const worker = createWorker({
   logger: (m) => console.log(m),
 });
 
-(async () => {
+const runOcr = async () => {
+  const img = document.getElementById("img");
   await worker.load();
   await worker.loadLanguage("eng");
   await worker.initialize("eng");
   const {
     data: { text },
-  } = await worker.recognize(
-    "https://tesseract.projectnaptha.com/img/eng_bw.png"
-  );
+  } = await worker.recognize(img);
+  ocrText.value = text;
   console.log(text);
-  await worker.terminate();
-})();
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+#img {
+  padding: 10px;
+}
+#result {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
